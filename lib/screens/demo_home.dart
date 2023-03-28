affee2394b05cc505740b176893fa10918d4ad8e
ademie <isijolaademie@gmail.com>
2023-03-28 04:05:34 -0600

    added lint rules in analysis options

---
+++ lib/screens/demo_home.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebaseauth/auth.dart';
+import 'package:flutter/material.dart';
+
+class DemoHome extends StatelessWidget {
+  DemoHome({super.key});
+
+  final User? user = Auth().currentUser;
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    body: Container(
+      child:const Text('Firebase Auth'),
+    ),
+    );
+  }
+}
