012042c42e8cb2303a43c995a74b410027443eb8
ademie <isijolaademie@gmail.com>
2023-03-28 03:53:45 -0600

    defined auth functions in auth.dart

---
+++ lib/auth.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Auth {
+  // init firebase auth
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  // create a user from firebase auth
+  User? get currentUser => _firebaseAuth.currentUser;
+
+  // listen to auth changes
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+
+  // create user
+  Future<void> register(
+      {required String email, required String password}) async {
+    await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  // login user
+  Future<void> login({required String email, required String password}) async {
+    await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+  }
+}
